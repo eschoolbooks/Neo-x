d63d7f5b6648ce9a207aeee32f9ae7073557439b
eschool books <eschoolbooksinfo@gmail.com>
2025-11-08 08:39:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "ema-eschool-ai-exam-forecaster",
+  "appId": "1:1080212946704:web:fe0ea361b6062be9eb2f95",
+  "apiKey": "AIzaSyBAAhgh1_QbCHRFQsmPbehUDI5us1zDAgY",
+  "authDomain": "ema-eschool-ai-exam-forecaster.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1080212946704"
+};
